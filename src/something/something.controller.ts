d74eb7aa2eeb0095e326362d1b2cb1f46e4b7aba
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2023-08-22 17:58:22 +0700

    feat(something.controller): inject configService

--- src/something/something.controller.ts
+++ src/something/something.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('something')
 export class SomethingController {
+  constructor(private configService: ConfigService) {}
   @Get()
   getHello() {
     return 'hello';
