bec7a82f3090764bd3f2ac94080912f245da31ac
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2023-08-25 15:54:13 +0700

    update(something.controller): remove configService

--- src/something/something.controller.ts
+++ src/something/something.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('something')
 export class SomethingController {
-  constructor(private configService: ConfigService) {}
   @Get()
-  getEnv() {
-    return this.configService.get<string>('ENV1');
+  getHello() {
+    return 'Hello';
   }
 }
