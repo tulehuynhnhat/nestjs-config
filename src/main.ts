4fd4febaedca7fc956e5586354fc6102b2cd12d6
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2023-08-29 10:14:12 +0700

    update(main): update using configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  await app.listen(port);
+  console.log(configService.get('port'));
+  console.log(configService.get('jwt.accessToken')); // or configService.get('JWT_ACCESS_TOKEN_SECRET')
+  console.log(configService.get('jwt.refreshToken')); // or configService.get('JWT_REFESH_TOKEN_SECRET')
+  console.log(configService.get('env1'));
+  console.log(configService.get('env2'));
 }
 bootstrap();
