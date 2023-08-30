042869a5f62ed7d9eb4c395a37b13577d086de78
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2023-08-30 10:21:43 +0700

    style(something.module)

--- src/something/something.module.ts
+++ src/something/something.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { SomethingController } from './something.controller';
 
 @Module({
-  controllers: [SomethingController]
+  controllers: [SomethingController],
 })
 export class SomethingModule {}
