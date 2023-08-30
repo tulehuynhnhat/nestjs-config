c642939132448b364697a9115858e183cc761f58
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2023-08-30 10:19:45 +0700

    update(app.module): import configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SomethingModule } from './something/something.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [SomethingModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SomethingModule,
+  ],
 })
 export class AppModule {}
