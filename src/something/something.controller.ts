fef88252495addc22f19df01d8219b5366ac129c
tulehuynhnhat <tulehuynhnhat05@gmail.com>
2023-08-30 10:20:58 +0700

    update(main, something): using configService

--- src/something/something.controller.ts
+++ src/something/something.controller.ts
@@ -1,9 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IConfig } from 'src/config/configuration';
 
 @Controller('something')
 export class SomethingController {
+  constructor(private configService: ConfigService<IConfig>) {}
   @Get()
-  getHello() {
-    return 'hello';
+  getENV() {
+    const env1 = this.configService.get<string>('env1');
+    const env2 = this.configService.get<string>('env2');
+    const port = this.configService.get<number>('port');
+    const jwtAccessToken = this.configService.get<string>('jwtAccessToken');
+    const jwtRefreshToken = this.configService.get<string>('jwtRefreshToken');
+
+    return {
+      env1,
+      env2,
+      port,
+      jwtAccessToken,
+      jwtRefreshToken,
+    };
   }
 }
